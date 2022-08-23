0f09bc6411cd0110c6703985fd962368441b41cb
OZiad <omar.ziad1@outlook.com>
2022-08-23 14:08:10 -0400

    Clean up startClock and take method out of frame

--- src/Frame.java
+++ src/Frame.java
@@ -11,56 +11,49 @@ public class Frame extends JFrame {
 	// Instance variables declaration
 	JLabel timeLabel;
 	JLabel dateLabel;
-	Calendar calendar;
 	SimpleDateFormat timeFormat;
 	SimpleDateFormat dateFormat;
-	String time;
-	String date;
+
 	Frame() {
 		this.setTitle("DigitalClock"); // Set frame title
 		this.setSize(600, 250); // Set frame size (width x height)
 		this.setResizable(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//set window to close when close button is pressed
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set window to close when close button is pressed
 		this.setLayout(new FlowLayout()); // JFrame layout
-		
-		
+
 		// time label visual representation
-		timeFormat = new SimpleDateFormat("hh:mm:ss a");	//time format set to hour:minute:seconds AM/PM
+		timeFormat = new SimpleDateFormat("hh:mm:ss a"); // time format set to hour:minute:seconds AM/PM
 		timeLabel = new JLabel();
 		timeLabel.setFont(new Font("Arial", Font.PLAIN, 70));
 		timeLabel.setForeground(Color.red);
 		timeLabel.setBackground(Color.black);
-		timeLabel.setOpaque(true);	//make background visible
-		
-		//date label visual representation
-		dateFormat = new SimpleDateFormat("E dd/MM/yyyy");		//date format set to day/month/year
+		timeLabel.setOpaque(true); // make background visible
+
+		// date label visual representation
+		dateFormat = new SimpleDateFormat("E dd/MM/yyyy"); // date format set to day/month/year
 		dateLabel = new JLabel();
 		dateLabel.setFont(new Font("Arial", Font.PLAIN, 70));
 		dateLabel.setForeground(Color.red);
 		dateLabel.setBackground(Color.black);
 		dateLabel.setOpaque(true);
-		
-		//Add labels to frame
+
+		// Add labels to frame
 		this.add(timeLabel);
 		this.add(dateLabel);
-		this.setVisible(true);	//make Frame window visible
-		
-		updateTime();
+		this.setVisible(true); // make Frame window visible
 	}
-	// method to update time every second
-	public void updateTime() {
-		while(true) {
-		time = timeFormat.format(Calendar.getInstance().getTime());	//store current time in time string
-		timeLabel.setText(time);
-		
-		date = dateFormat.format(Calendar.getInstance().getTime());	//store current time in time string
-		dateLabel.setText(date);
-	    try {
-			Thread.sleep(1000);	// sleep method until next second
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 
-	}
+	// method to update time every second
+	public void startClock() {
+		while (true) {
+			timeLabel.setText(timeFormat.format(Calendar.getInstance().getTime()));
+			dateLabel.setText(dateFormat.format(Calendar.getInstance().getTime()));
+			try {
+				Thread.sleep(1000); // sleep method until next second
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
 }
